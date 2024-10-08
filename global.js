faf3823deda9e92608d0aa84c745985f45d3755e
qasimali1st <157275129+qasimali1st@users.noreply.github.com>
2024-10-08 13:46:31 +0500

    Update global.js

--- global.js
+++ global.js
@@ -322,14 +322,13 @@ global.reloadHandler = async function (restatConn) {
   return true
 }
 
-
 const pluginFolder = global.__dirname(join(__dirname, './plugins/index'))
 const pluginFilter = filename => /\.js$/.test(filename)
 global.plugins = {}
 async function filesInit() {
-  for (let filename of readdirSync(pluginFolder).filter(pluginFilter)) {
+  for (const filename of readdirSync(pluginFolder).filter(pluginFilter)) {
     try {
-      let file = global.__filename(join(pluginFolder, filename))
+      const file = global.__filename(join(pluginFolder, filename))
       const module = await import(file)
       global.plugins[filename] = module.default || module
     } catch (e) {
@@ -338,30 +337,36 @@ async function filesInit() {
     }
   }
 }
-filesInit().then(_ => console.log(Object.keys(global.plugins))).catch(console.error)
+filesInit()
+  .then(_ => Object.keys(global.plugins))
+  .catch(console.error)
 
 global.reload = async (_ev, filename) => {
   if (pluginFilter(filename)) {
-    let dir = global.__filename(join(pluginFolder, filename), true)
+    const dir = global.__filename(join(pluginFolder, filename), true)
     if (filename in global.plugins) {
-      if (existsSync(dir)) conn.logger.info(`🌟 Plugin Actualizado - '${filename}'`)
+      if (existsSync(dir)) conn.logger.info(`\nUpdated plugin - '${filename}'`)
       else {
-        conn.logger.warn(`🗑️ Plugin Eliminado - '${filename}'`)
+        conn.logger.warn(`\nDeleted plugin - '${filename}'`)
         return delete global.plugins[filename]
       }
-    } else conn.logger.info(`✨ Nuevo plugin - '${filename}'`)
-    let err = syntaxerror(readFileSync(dir), filename, {
+    } else conn.logger.info(`\nNew plugin - '${filename}'`)
+    const err = syntaxerror(readFileSync(dir), filename, {
       sourceType: 'module',
-      allowAwaitOutsideFunction: true
+      allowAwaitOutsideFunction: true,
     })
-    if (err) conn.logger.error(`syntax error while loading '${filename}'\n${format(err)}`)
-    else try {
-      const module = (await import(`${global.__filename(dir)}?update=${Date.now()}`))
-      global.plugins[filename] = module.default || module
-    } catch (e) {
-      conn.logger.error(`error require plugin '${filename}\n${format(e)}'`)
-    } finally {
-      global.plugins = Object.fromEntries(Object.entries(global.plugins).sort(([a], [b]) => a.localeCompare(b)))
+    if (err) conn.logger.error(`\nSyntax error while loading '${filename}'\n${format(err)}`)
+    else {
+      try {
+        const module = await import(`${global.__filename(dir)}?update=${Date.now()}`)
+        global.plugins[filename] = module.default || module
+      } catch (e) {
+        conn.logger.error(`\nError require plugin '${filename}\n${format(e)}'`)
+      } finally {
+        global.plugins = Object.fromEntries(
+          Object.entries(global.plugins).sort(([a], [b]) => a.localeCompare(b))
+        )
+      }
     }
   }
 }
