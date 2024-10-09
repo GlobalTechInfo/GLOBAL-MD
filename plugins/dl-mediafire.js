61d002fec12c481de7a446a54de4e79cc310254e
qasimali1st <157275129+qasimali1st@users.noreply.github.com>
2024-10-10 02:58:19 +0500

    V3

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -2,33 +2,34 @@ import fetch from 'node-fetch'
 import { mediafiredl } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, args, usedPrefix, command, isOwner, isPrems }) => {
-  var limit
-  if (isOwner || isPrems) limit = 1200
-  else limit = 100
-  if (!args[0]) throw `✳️ Enter the mediafire link next to the command`
-  if (!args[0].match(/mediafire/gi)) throw `❎ Link incorrect`
-  m.react(rwait)
-  let full = /f$/i.test(command)
-  let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
-  let ss = await (await fetch(`https://image.thum.io/get/fullpage/${u}`)).buffer()
-  let res = await mediafiredl(args[0])
-  let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
-  let isLimit = (isPrems || isOwner ? limit : limit) * 1012 < filesize
-  let caption = `
-   ≡ *MEDIAFIRE*
+  let limit = isOwner || isPrems ? 1200 : 100;
+  if (!args[0]) throw `✳️ Enter the mediafire link next to the command`;
+  if (!args[0].match(/mediafire/gi)) throw `❎ Link incorrect`;
+  m.react('⌛');
 
-▢ *Number:* ${filename}
+  let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0];
+  let ss = await (await fetch(`https://image.thum.io/get/fullpage/${u}`)).buffer();
+  let res = await mediafiredl(args[0]);
+  let { url, filename, ext, aploud, filesize, filesizeH } = res;
+  
+  let isLimit = (isPrems || isOwner ? limit : limit) * 1024 < filesize;
+  let caption = `
+  ≡ *MEDIAFIRE*
+
+▢ *Name:* ${filename}
 ▢ *Size:* ${filesizeH}
 ▢ *Extension:* ${ext}
 ▢ *Uploaded:* ${aploud}
-${isLimit ? `\n▢ The file exceeds the download limit *+${limit} MB*\nUpgrade to premium to be able to download files more than *900 MB*` : ''} 
-`.trim()
-  await conn.sendFile(m.chat, ss, 'ssweb.png', caption, m)
+${isLimit ? `\n▢ The file exceeds the download limit of *${limit} MB*. Upgrade to premium to download larger files.` : ''}`;
+  
+  await conn.sendFile(m.chat, ss, 'ssweb.png', caption, m);
 
   if (!isLimit)
-    await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
-  m.react(done)
-}
+    await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true });
+  
+  m.react('✅');
+};
+
 handler.help = ['mediafire <url>']
 handler.tags = ['downloader', 'premium']
 handler.command = ['mediafire', 'mfire']
