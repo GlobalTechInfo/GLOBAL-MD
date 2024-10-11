e23f8d76255bccc8b8f1c354df26c9dfaa89449a
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-10-11 22:46:02 +0500

    > V3

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,14 +1,21 @@
-function handler(m) {
-  const data = global.botNumber.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(
-    m.chat,
-    data.map(([id, name]) => [id, name]),
-    m
-  )
-}
+let handler = async function (m, { conn, text }) {
+  const botNumbers = global.owner.map(owner => owner[0]);
+  botNumbers.push(global.botNumber); // Include botNumber
+  
+  const uniqueNumbers = [...new Set([...botNumbers, ...global.mods])];
+  const commonNumbers = uniqueNumbers.filter(number => 
+    botNumbers.includes(number) && global.mods.includes(number) && global.owner.some(owner => owner[0] === number)
+  );
 
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño', 'Gowner']
+  if (commonNumbers.length > 0) {
+    conn.reply(m.chat, `❇️ Here Is The Contact Of My Owner: ${commonNumbers.join(', ')}`, m);
+  } else {
+    conn.reply(m.chat, `You are not recognized as a bot owner.`, m);
+  }
+};
 
-export default handler
+handler.help = ['owner'];
+handler.tags = ['main'];
+handler.command = /^owner$/i;
+
+export default handler;
