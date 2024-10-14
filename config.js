1a9bcc33234795bc320a1ccc19badfc88d34c063
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-10-15 01:32:56 +0500

    > V3

--- config.js
+++ config.js
@@ -28,14 +28,26 @@ global.keysxteammm = [
   '29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb',
   '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63'
 ];
-global.keysxteam = keysxteammm[Math.floor(Math.random() * keysxteammm.length)];
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.lolkeysapi = ['GataDios']
 
-global.keysneoxrrr = ['5VC9rvNx', 'cfALv5'];
-global.keysneoxr = keysneoxrrr[Math.floor(Math.random() * keysneoxrrr.length)];
-
-global.lolkeysapi = ['GataDios'];
-global.canal = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07';
-
+global.APIs = {
+  // API Prefix
+  // name: 'https://website'
+  xteam: 'https://api.xteam.xyz',
+  dzx: 'https://api.dhamzxploit.my.id',
+  lol: 'https://api.lolhuman.xyz',
+  violetics: 'https://violetics.pw',
+  neoxr: 'https://api.neoxr.my.id',
+  zenzapis: 'https://zenzapis.xyz',
+  akuari: 'https://api.akuari.my.id',
+  akuari2: 'https://apimu.my.id',
+  nrtm: 'https://fg-nrtm.ddns.net',
+  bg: 'http://bochil.ddns.net',
+  fgmods: 'https://api-fgmods.ddns.net',
+}
 global.APIKeys = {
   // APIKey Here
   // 'https://website': 'apikey'
@@ -45,31 +57,35 @@ global.APIKeys = {
   'https://violetics.pw': 'beta',
   'https://zenzapis.xyz': `${keysxxx}`,
   'https://api-fgmods.ddns.net': 'fg-dylux',
-};
+}
 
 // Sticker WM
-global.botname = 'GLOBAL-MD';
-global.premium = 'true';
-global.packname = 'GLOBAL BOT';
-global.author = 'GlobalTechInfo';
-global.igfg = 'https://instagram.com/global.techinc';
-global.fgsc = 'https://github.com/GlobalTechInfo/GLOBAL-MD';
-global.fgyt = 'https://youtube.com/@GlobalTechInfo';
-global.fgpyp = 'https://youtube.com/@GlobalTechInfo';
-global.fglog = 'https://i.ibb.co/GVS5Dy9/Ephoto360-com-167054c3328030-2.jpg';
-global.thumb = fs.readFileSync('./assets/qasim.jpg');
-global.wait = 'Loading........';
-global.rwait = '⌛';
-global.dmoji = '';
-global.done = '✅';
-global.error = '❌';
-global.xmoji = '';
-global.multiplier = 69;
-global.maxwarn = '3';
+global.botname = process.env.BOTNAME
+global.premium = 'true'
+global.packname = 'GLOBAL'
+global.author = 'Qasim'
+global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/global.techinfo\n'
+global.dygp = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.fgsc = 'https://github.com/GlobalTechInfo/GLOBAL-MD'
+global.fgyt = 'https://youtube.com/@GlobalTechInfo'
+global.fgpyp = 'https://youtube.com/@GlobalTechInfo'
+global.fglog = 'https://i.ibb.co/G2dh9cB/qasim.jpg'
+global.thumb = fs.readFileSync('./assets/qasim.jpg')
 
-let file = fileURLToPath(import.meta.url);
+global.wait = '*⌛ _Loading..._*'
+global.rwait = '⌛'
+global.dmoji = '🤭'
+global.done = '✅'
+global.error = '❎'
+global.xmoji = '🔥'
+
+global.multiplier = 69
+global.maxwarn = '3'
+
+let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
-  unwatchFile(file);
-  console.log(chalk.redBright("Update 'config.js'"));
-  import(`${file}?update=${Date.now()}`);
-});
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
