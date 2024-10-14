11b5ffc85ea55963a6f84fafa1893f7cd042a0e4
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-10-15 01:56:30 +0500

    > V3

--- plugins/Hello.js
+++ plugins/Hello.js
@@ -1,29 +1,39 @@
 export async function all(m) {
-  // when someone sends you hello message
+  const greetings = [
+    'Hello', 'Hi', 'Mambo', 'Oy', 'Niaje', 'kaka',
+    'Hola', 'Salut', 'Bonjour', 'مرحبا', 'أهلاً', 
+    'Selamat', 'Halo', 'Kamusta', 'Mabuhay', 'Magandang', 
+    '你好', '嗨', '您好', 'Mhoro', 'Mhoroi', 'Salibonani', 
+    'Lumkela', 'Jambo', 'Habari', 'Sawubona', 'Dumela', 
+    'Kgotso', 'Kedu', 'Nnoo', 'Bawo ni', 'Ek’aaro', 
+    'Muli bwanji', 'Tere', 'Hei', 'Moi', 'Privet', 
+    'Hallo', 'Guten Tag', 'Ciao', 'Hej', 'Hallå', 'Olá', 'Oi', 'Salut'
+  ];
+
   if (
-    (m.mtype === 'hellomessage' ||
-      m.text.startsWith('Hello') ||
-      m.text.startsWith('Hi') ||
-      m.text.startsWith('Mambo') ||
-      m.text.startsWith('Oy') ||
-      m.text.startsWith('Niaje') ||
-      m.text.startsWith('kaka')) &&
+    greetings.some(greeting => m.text.startsWith(greeting)) &&
     !m.isBaileys &&
     !m.isGroup
- /* ) {
-    this.sendMessage(
-      m.chat,
-      {
-        text: `Hello @${m.sender.split('@')[0]}\nyou can rent the bot to join a group\n\n_For more info you can DM the owner_\n*Type* \`\`\`.owner\`\`\` *to contact the owner*`.trim(),
-      },
-      { quoted: m }*/
-    ) {
-    this.sendButton(m.chat, `*WELCOME ITS ME JUST REPLYING*      
-    morning or evening @${m.sender.split('@')[0]} 
-    i may be offline or i may be slow to respond you but wait i will be back soon 😇\n\n *What we offer*\n\n1. Heroku credit cards\n2. Bot deployment works 24/7\n3. Social media followers\n\n\n> click the buttons to see me
-  `.trim(), igfg, null, [['OWNER HELP', '.mrcs'],['GET TEXT', '.repo']] , m, { mentions: [m.sender] })
-    m.react('🤫')
-  }
+  ) {
+    this.sendButton(
+      m.chat, 
+      `*WELCOME, IT'S BOT JUST REPLYING*
+      Morning or Evening @${m.sender.split('@')[0]}
+      I May Be Offline Or May Be Slow To Respond You
 
-  return !0
+      *What We Offer*
+      1. Pure Love
+      2. Bot Deployment Tutorials
+      3. Free Bot Script
+
+      > click the buttons to see more`.trim(),
+      igfg,
+      null, 
+      [['OWNER HELP', '.mrcs'], ['GET TEXT', '.repo']], 
+      m,
+      { mentions: [m.sender] }
+    );
+    m.react('');
+  }
+  return !0;
 }
