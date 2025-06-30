67b2643f32d88ee11635db75d13f9ec350ac6a80
GlobalTechInfo <raikasim1@gmail.com>
2025-06-30 15:44:42 +0000

    Clean

--- README.md
+++
@@ -1,7 +0,0 @@
-
-
-# REPO CLOSED || TAP ON FOLLOWING BUTTON TO FORK NEW REPO
-
-
-  <a href="https://github.com/GlobalTechInfo/ULTRA-MD/fork"><img title="ULTRA-MD" height="200" width="470" src="https://img.shields.io/badge/FORK-ULTRA MD-h?color=blue&style=for-the-badge&logo=stackshare"></a>
-  
--- plugins/main-creator.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;qasim;;\nFN:qasim\nORG:qasim\nTITLE:\nitem1.TEL;waid=255734980103:255734980103\nitem1.X-ABLabel:qasim\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:qasim\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'Qasim Ali', contacts: [{ vcard }] }}, {quoted: m})
-}
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
-
-export default handler
